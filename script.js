684afb7c253c5fea1451f0cf2ae8c25b1c4bb71c
RCSchornick <94027300+RCSchornick@users.noreply.github.com>
2021-12-27 00:34:11 -0600

    edited jsscript and html, finalized project

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 //Todays Date Displayed at the top of the calendar
 var todayDate = moment().format ('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
-for (var i=9; i < 18; i++) {
-    //creating 8 blocks
-    var row = $(`<div data-time=${i} id='${i}' class="row">`);
-    //time block 9-5
-    var time = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>');
-    
-}
-
-var displayDay = function() {
-    $("#currentDay").text(currentDay);
-};
+var currentHour = moment().hours()
 
 
+$(".saveBtn").on("click",function(){
+    var value = $(this).siblings(".description").val()
+    var timeKey = $(this).parent().attr("id")
+localStorage.setItem(timeKey,value)
 
-
-
-//local storage
-$(".saveBtn").on("click", function(){
-    var selection = $(this).siblings("description").val().trim();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, selection);
 })
 
-//Pull time from local storage
-$("#9AM .description").val(localStorage.getItem("9AM"));
-$("#10AM .description").val(localStorage.getItem("10AM"));
-$("#11AM .description").val(localStorage.getItem("11AM"));
-$("#12AM .description").val(localStorage.getItem("12AM"));
-$("#1AM .description").val(localStorage.getItem("1AM"));
-$("#2AM .description").val(localStorage.getItem("2AM"));
-$("#3AM .description").val(localStorage.getItem("3AM"));
-$("#4AM .description").val(localStorage.getItem("4AM"));
-$("#5AM .description").val(localStorage.getItem("5AM"));
+
+$("#9 .description").val(localStorage.getItem("9"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
+
+
+function updateCss(){
+    $(".time-block") .each(function(){
+        var divTime = $(this).attr("id")
+        if(divTime > currentHour){
+            $(this).addClass("future")
+        }
+        else if(divTime === currentHour){
+            $(this).addClass("present")
+        }
+        else if(divTime < currentHour){
+            $(this).addClass("past")
+        }
+
+    })
+}
+updateCss()
